45c33d739fcb8197bb537195dbfee640f2d09d77
loudsheep <loudsheep05@gmail.com>
2024-03-26 18:23:28 +0100

    Fixed wrong event in modal onClose

--- resources/js/Pages/BindBoard/Show.jsx
+++ resources/js/Pages/BindBoard/Show.jsx
@@ -105,7 +105,7 @@ export default function Show({ auth, bindboard, binds, canAddMoreBinds, canPlayB
                         <h3 className="text-xl font-bold text-gray-900">
                             {bindToPlay?.name}
                         </h3>
-                        <button type="button" className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="authentication-modal" onClick={onCloseNewBindModal}>
+                        <button type="button" className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center" data-modal-hide="authentication-modal" onClick={onClosePlayBindModal}>
                             <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                             </svg>
