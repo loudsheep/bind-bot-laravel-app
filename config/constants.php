6116adcf78a397a07fa3ccaea6dce6077045b356
loudsheep <loudsheep05@gmail.com>
2024-04-01 17:44:43 +0200

    Fixed permissions issue

--- config/constants.php
+++ config/constants.php
@@ -7,9 +7,6 @@ return [
         'EDIT_BIND' => 4,
         'DELETE_BIND' => 8,
         'PLAY_BIND_ON_SERVER' => 16,
-        // 'CREATE_INVITES' => 32,
-        // 'DELETE_INVITES' => 64,
-        // 'REMOVE_PARTICIPANTS' => 128,
-        'ADMIN' => 255 + 256,
+        'ADMIN' => 256,
     ],
 ];
